d7fcb05037ef27f0cc51c5531b49241fa59155cc
alissonjosef <alissonjosef@gmail.com>
2024-05-16 00:51:30 -0300

    validação

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -11,19 +11,23 @@ public class ContaTerminal {
         Scanner scanner = new Scanner(System.in);
         ContaTerminal conta = new ContaTerminal();
 
-        System.out.println("Por favor, digite o número da Agência:");
-        String agenciaDigitada = scanner.nextLine();
+        boolean agenciaValida = false;
 
-        if (agenciaDigitada.equals(conta.agencia)) {
-            System.out.println("Agência encontrada.");
-        } else {
-            System.out.println("Agência não encontrada.");
+        while (!agenciaValida) {
+            System.out.println("Por favor, digite o número da Agência:");
+            String agenciaDigitada = scanner.nextLine();
+
+            if (agenciaDigitada.equals(conta.agencia)) {
+                agenciaValida = true;
+                System.out.println("Agência encontrada.");
+
+                System.out.println("Olá " + conta.nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                        + conta.agencia + ", conta " + conta.numero + " e seu saldo " + conta.saldo
+                        + " já está disponível para saque.");
+            } else {
+                System.out.println("Agência não encontrada. Por favor, digite novamente.");
+            }
         }
-
-
-        System.out.println("Olá " + conta.nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
-                + conta.agencia + ", conta " + conta.numero + " e seu saldo " + conta.saldo
-                + " já está disponível para saque.");
 
         scanner.close();
     }
